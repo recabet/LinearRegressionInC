bec07e25dc0afbe1a1d045dcf3e324a6a907c072
recabet <rajab.iskenderli@gmail.com>
2024-06-22 15:44:30 +0400

    Minor changes

--- linear_regression.h
+++ linear_regression.h
@@ -1,21 +1,21 @@
 //
-// Created by rajab on 12/06/24.
+// Created by Recabet on 12/06/24.
 //
 
-#ifndef LINEARREGRESSIONINC_LINEAR_REGRESSION_H
-#define LINEARREGRESSIONINC_LINEAR_REGRESSION_H
+#ifndef LINEAR_REGRESSION_H
+#define LINEAR_REGRESSION_H
 
 #include <stddef.h>
 
-extern float (*train_data)[2];
+extern float (* train_data)[2];
 
 extern size_t train_size;
 
-float randf ();
+float randf();
 
-float cost (float coeff, float bias);
+float cost(float coeff, float bias);
 
-float *train (float coeff, float bias, float epsilon, float rate, size_t iterations);
+float* train(float coeff, float bias,float  epsilon, float rate, size_t iterations);
 
 
-#endif //LINEARREGRESSIONINC_LINEAR_REGRESSION_H
+#endif //LINEAR_REGRESSION_H
--- main.c
+++ main.c
@@ -2,44 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 //Some example usage
 
-
-
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
+	//Example  starting weight and bias.
 	srand(69);
-
+	float k = randf() * 10;
+	float b = randf() * 10;
 	//Example data
-	float example_data[][2] = {
-			{0, 0},
-			{1, 2},
-			{2, 4},
-			{3, 6},
-			{4, 8}
-	};
+	float example_data[][2] = {{0, 0},
+							   {1, 2.3},
+							   {2, 4.2},
+							   {3, 7.3},
+							   {4, 10.2}};
 
 	train_size = sizeof(example_data) / sizeof(example_data[0]);
 	train_data = malloc(train_size * sizeof(example_data[0]));
-	if (train_data == NULL)
+	if(train_data == NULL)
 	{
 		fprintf(stderr, "Memory allocation failed\n");
 		return 1;
 	}
-
 	// Copy example data to train_data
-	for (size_t i = 0; i < train_size; i++)
+	for(size_t i = 0; i < train_size; i++)
 	{
 		train_data[i][0] = example_data[i][0];
 		train_data[i][1] = example_data[i][1];
 	}
-	//Example  starting weight and bias.
-	float k = randf() * 10;
-	float b = randf() * 5;
 
-	float* result = train(k, b, 1e-3, 1e-3, 50000);
-	if (result != NULL)
+	float* result = train(k, b, 1e-2, 1e-2, 5000);
+
+	if(result != NULL)
 	{
 		printf("Coefficient: %f, Bias: %f\n", result[0], result[1]);
 		free(result);
